b44e0495d6b07010b0f694a0823a1981ca8d648c
geta95 <57476459+geta95@users.noreply.github.com>
2021-02-20 16:26:05 +0900

    Update Fill.asm

--- project/04/fill/Fill.asm
+++ project/04/fill/Fill.asm
@@ -12,10 +12,10 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
-(BLACK)   // if press any button on keyboard
   @i
   M=1
-  
+
+(BLACK)   // if press any button on keyboard
   @i
   D=M
   
