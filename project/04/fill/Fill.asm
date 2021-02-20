868f57485ddd7e4137da8007c9fe47fc281c94ea
geta95 <57476459+geta95@users.noreply.github.com>
2021-02-20 16:18:09 +0900

    Update Fill.asm

--- project/04/fill/Fill.asm
+++ project/04/fill/Fill.asm
@@ -12,8 +12,32 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(BLACK)   // if press any button on keyboard
+  @i
+  M=1     // i = 1
+  
+  @i
+  D=M     // D = i
+  
+  @8196
+  D=D-A   // D = i - 8196
+  
+  @END
+  D;JGT   // if D = i - 8196 jmp BLACK
+  
   @SCREEN
   M=-1
+  
+  @i
+  D=M+1
+  
+  @SCREEN
+  A=A+D
+  M=-1
+  
+  @BLACK
+  0;JMP
+  
 (END)
   @END
   0;JMP
